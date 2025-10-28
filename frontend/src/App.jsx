4943561d283c605e0844fac20d42703dc1d1b56e
Harrison Stein <hstein@smu.edu>
2025-10-28 15:12:13 -0500

    React build

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1>Hello, this is a test of our webpage!</h1>
+      <p>React + Bootstrap is a success.</p>
+
+      {/* This is a React-Bootstrap component */}
+      <Button variant="primary">Click Me!</Button>
+    </div>
+  )
+}
+
+export default App
--- frontend/src/services/README.md
+++
@@ -1,2 +0,0 @@
-All Python + Database components.
-
