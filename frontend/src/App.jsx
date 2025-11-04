1f797ada0582b0939a0c8c45335d099e8f4c926a
Harrison Stein <hstein@smu.edu>
2025-11-04 15:41:04 -0600

    pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,25 @@
 import Cover from './Cover.jsx'
+// For switching between pages
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import './assets/cover.css'
+import { Home } from './Pages/home'
+import { Page1 } from './pages/Page1.jsx'
+import { Page2 } from './pages/Page2.jsx'
+import { Page3 } from './pages/Page3.jsx'
 
 function App() {
   return <Cover />
+
+  return {
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/page1" element={<Page1/>}/>
+        <Route path="/page2" element={<Page2/>}/>
+        <Route path="/page3" element={<Page3/>}/>
+        </Routes>
+    </Router>
+  }
 }
 
 export default App
